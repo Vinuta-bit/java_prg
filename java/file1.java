9c4482c130443e0748d740725dfdddcdeb7333a5
Vinuta-bit <vinutasinnur22@gmail.com>
2024-11-30 09:07:30 +0530

    Update file1.java

--- java/file1.java
+++ java/file1.java
@@ -1,9 +1,21 @@
-class file1 { 
-    public static void main(String[] args) 
-    { int n = 5; for (int i = 1; i <= n; i++) 
-    { for (int j = 1; j <= n; j++) { if (i == j) 
-    { System.out.print("* ");
-    } 
-    else { System.out.print(" "); 
-         } }
-     System.out.println(); } } }
+class file1
+{
+    public static void main(String[] args)
+    {
+        int n=5;
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n;j++)
+            {
+            if((i+j<=n+1)){
+                System.out.print("* ");
+            }
+            else
+            {
+                System.out.print("  ");
+            }
+        }
+        System.out.println();
+    }
+}
+}
